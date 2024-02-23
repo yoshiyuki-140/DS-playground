5b439507b1b94ba403cdc07aa7ad4c31100c20ce
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-02-23 12:34:37 +0900

    add : サンプルデータのロード

--- ch4/categoricalData_preprocessing_002.py
+++ ch4/categoricalData_preprocessing_002.py
@@ -3,6 +3,12 @@
 # 名義特徴量のエンコード
 
 import numpy as np
+import pickle
+
+# サンプルデータのロード
+load_file_path = './sample_dataFrame_002.pkl'
+with open(load_file_path, mode='rb') as f:
+    df = pickle.load(f)
 
 # クラスラベルと整数を対応させる辞書を生成
-class_mapping = {lable: idx for idx, labl in enumerate(np.unique(df['']))}
+# class_mapping = {lable: idx for idx, labl in enumerate(np.unique(df['size']))}
