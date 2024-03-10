44f44f5d28e5bc4de5a3869dafebe9bce2195ddc
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-10 12:52:04 +0900

    add : ch6

---
+++ ch6/plot_learning_curve.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
+
+# 正規化を行うためのモジュール
+from sklearn.preprocessing import StandardScaler
+
+# パイプラインを構築するためのモジュール
+from sklearn.pipeline import make_pipeline
